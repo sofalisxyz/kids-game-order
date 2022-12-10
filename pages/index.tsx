c4018171d183b4801fbbf8938611da020d1c46a8
Sophia Lis <h11d3nname@gmail.com>
2022-12-10 05:07:50 +0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Тренажер &#34;Порядок&#34;</title>
+      </Head>
+      test
+    </div>
+  );
+}
